dac737f2be7321452a09c70d5b2831e317c6e9cf
oguzhan <oguzhanngoley@gmail.com>
2024-03-11 16:10:45 +0300

    Initial

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+        public ProductsController(StoreContext storeContext)
+        {
+            context = storeContext;
+        }
+
+        [HttpGet]
+        public async Task< ActionResult<List<Product>>> GetProducts(){
+            var products = await context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+         [HttpGet("{id}")]
+        public async  Task<ActionResult<Product>> GetProductById(int id){
+            var product = await context.Products.FirstOrDefaultAsync(x=>x.Id == id);
+            return Ok(product);
+        }
+    }
+}
